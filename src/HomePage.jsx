e7683d7149128a371eb96cbb1bc68db5b24d171e
tom dav <kobe242tomy@icloud.com>
2023-09-05 18:30:10 +0200

    HomePage bacground

---
+++ src/HomePage.jsx
@@ -0,0 +1,16 @@
+import "./moon.css";
+
+function HomePage() {
+  return (
+    <div id="root">
+      <div className="layer layer-1"></div>
+      <div className="layer layer-2"></div>
+      <div className="layer layer-3"></div>
+      <div className="layer layer-4"></div>
+      <div className="layer layer-5"></div>
+      <div className="layer layer-6"></div>
+    </div>
+  );
+}
+
+export default HomePage;
